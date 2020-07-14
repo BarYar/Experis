c5aafe550f62c452a08241b686cad64fe860cfa8
BarYar <68278295+BarYar@users.noreply.github.com>
2020-07-14 16:22:31 +0300

    Barr

--- venv/Book/Ex54.py
+++ venv/Book/Ex54.py
@@ -1,4 +1,4 @@
-L=[input("type 2 lists")]
+L=[input("type 2 lists...")]
 L1=[input()]
 L2=L+L1
 print(L2)
--- venv/Book/Loop5Ex3.py
+++ venv/Book/Loop5Ex3.py
@@ -1,4 +1,5 @@
 num = int(input("הקלד את גודל המשולש."))
+print ("hey")
 for i in range (0,num):
     for j in range (0, num):
         if i > j:
